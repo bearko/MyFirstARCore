a623cbe067e663aaecce993be728a9e0eceeb691
Yohei Miyamoto <bearko.miyamoto@gmail.com>
2017-09-09 16:03:57 +0900

    MyFirstARCore
    Created

---
+++ Mokumoku_20170908/Assets/GoogleARCore/HelloARExample/Scripts/Button_Bed.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClickExample : MonoBehaviour {
+	public Button yourButton;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		GameObject.Find ("ExampleController").GetComponent<Hello AR Controller> ().Andy Android Prefab = true;
+	}
+}
